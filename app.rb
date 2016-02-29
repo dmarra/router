9adaf4403cc0f3deb7d1d1446042a1dbe3847002
dmarra <dmarra@gcinfotech.com>
2016-02-28 20:51:13 -0500

    Improves performance of distance request by an order of magnitude by batching requests into 60 query chunks. NOTE: was tested with a standard address; had trouble getting spherical coordinates from nekudo in local development environment. Please test to ensure destination is working.

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ ALL_OFFICES = ALL_OFFICES.to_a.map {|row| row.to_hash }.freeze
 
 class Router < Sinatra::Base
   Gistance.configure do |c|
+    c.api_key = 'AIzaSyDO757jP_0OhC_0yDWDbb1q6_or9Vdyf1I'
     c.units = 'imperial'
   end
 
@@ -20,24 +21,48 @@ class Router < Sinatra::Base
 
   get '/' do
     # get current location
+    # NOTE: should we use the zip code they enter? 
+    #       what if they sigfn up from someplace that is not home?
     @current_location = RestClient.get("http://geoip.nekudo.com/api/#{request.ip}")
     @current_location = JSON.parse(@current_location)['location']
 
     # get campaign office distance
-    @all_offices = ALL_OFFICES.dup
-    @all_offices.each do |office|
+    @all_offices = ALL_OFFICES.dup 
+
+    # there is a max size to a request. our list is large enough that
+    # we need to split this request into chunks. 60 seems to work; 99
+    # was too high.     
+    @distances = Array.new  
+    index = 0
+    @all_offices.each_slice(60).with_index do |sub_list, i|
+      @origins = Array.new
+
+      sub_list.each_with_index do |office, j|        
+        @origins.push("#{office[:lat]},#{office[:lon]}")
+      end
+
       distance_matrix = Gistance.distance_matrix(
         destinations: ["#{@current_location['latitude']},#{@current_location['longitude']}"],
-        origins: ["#{office[:lat]},#{office[:lon]}"]
-      )
-      office[:distance_int] = distance_matrix['rows'][0]['elements'][0]['distance']['value']
-      office[:distance_text] = distance_matrix['rows'][0]['elements'][0]['distance']['text']
-    end
+        origins: @origins
+      )    
+
+      # add results to array of distances for sorting              
+      distance_matrix.rows.each do |row|
+        row.elements.each do |element|       
+          @distances.push({:index => index ,:distance => element.distance.value})
+          index += 1
+        end
+      end     
+    end 
 
     # sort by closest
-    @all_offices.sort_by! { |v| v[:distance_int].to_i }
-    @nearest_campaign = @all_offices.first
-
+    @distances.sort_by! do |distance|
+      distance[:distance].to_i
+    end
+     
+    # tada!
+    @nearest_office = @all_offices[@distances.first[:index]]   
+    
     erb :index
   end
 
